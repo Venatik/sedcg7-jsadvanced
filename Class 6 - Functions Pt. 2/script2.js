19e497d3bc266e4827de25dc7922ec94505befed
Venatik <stefan.trajkovski008@gmail.com>
2024-01-30 19:02:37 +0100

    modified:   Class 4 - Functions Pt. 1/index.html 	modified:   Class 6 - Functions Pt. 2/index.html 	modified:   Class 6 - Functions Pt. 2/script2.js

--- Class 6 - Functions Pt. 2/script2.js
+++ Class 6 - Functions Pt. 2/script2.js
@@ -89,8 +89,25 @@ let result = numbers
 
 console.log(result);
 
+console.log("============== REDUCE Part 2 ==============");
+
+// Without a higher order function
+function addNumbers() {
+    let sum = 0;
+
+    for (let i = 0; i < numbers.length; i++) {
+        sum += numbers[i];
+    }
+
+    return sum;
+}
+console.log(addNumbers());
+
+// With a higher order function
+let sumReduceHigher = numbers.reduce((total, number) => total + number);
+console.log(sumReduceHigher);
+
 // ============== SORT ==============
-// 
 
 console.log("============== SORT ==============");
 // with .sort we will sort the elements of the array in descending and an ascending order
@@ -103,3 +120,126 @@ console.log("sorted array - Descending", numbersDescending);
 
 let numbersAscending = numbersDescending.sort((first, second) => first - second);
 console.log("Ascending", numbersAscending);
+
+console.log("============== SORT Part 2 ==============");
+
+// Without a higher order function
+function sortFunction(arr) {
+    for (let i = 0; i < arr.length; i++) {
+        for (let j = i + 1; j < arr.length; j++) {
+            if (arr[i] < arr[j]) {
+                let temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
+        }
+    }
+    return arr;
+}
+console.log(`Before sort: ${numbers}`);
+console.log(`After sort: ${sortFunction(numbers)}`);
+
+// With a higher order function
+let numbersAscendingSort = numbers.sort((first, second) => first - second);
+console.log(numbersAscendingSort);
+
+// ============== forEach ==============
+
+console.log("============== forEach ==============");
+
+numbers.forEach(number => console.log(number + 3));
+
+// ============== EVERY ==============
+
+console.log("============== EVERY ==============");
+// .every() tests every value from the array and returns true or false
+
+let biggerThanTwo = numbers.every(num => num > 2);
+console.log(biggerThanTwo);
+
+// ============== SOME ==============
+
+console.log("============== SOME ==============");
+// .some()
+
+let someBiggerThanTwo = numbers.some(num => num > 2);
+console.log(someBiggerThanTwo);
+
+// ============== FIND ==============
+
+console.log("============== FIND ==============");
+
+let cityArray = ["Skopje", "Veles", "Kumanovo", "Tetovo", "Bitola"];
+
+let findWord = cityArray.find(word => word === "Skopje");
+console.log(findWord);
+
+// ============== FIND INDEX ==============
+
+console.log("============== FIND INDEX ==============");
+
+let findIndex = cityArray.findIndex(word => word === "Kumanovo");
+console.log(findIndex);
+
+// ============== INCLUDES ==============
+
+console.log("============== INCLUDES ==============");
+// .includes() returns true or false
+
+let includesPrilep = cityArray.includes("Prilep");
+console.log(includesPrilep);
+
+// ============== FLAT ==============
+
+console.log("============== FLAT ==============");
+// .flat() - flattens the array
+
+let newArray = [1, 2, [3, 4, [5, 6]]];
+
+let flattenedArray = newArray.flat(2); // 2 is the depth of the array
+console.log(newArray);
+
+// ============== FLAT MAP ==============
+
+console.log("============== FLAT MAP ==============");
+// .flatMap() - flattens the array and maps it
+
+let flatMappedArray = cityArray.flatMap(word => word.split("")); // Only .map() will return an array of arrays. Only .flat() will flatten the array. .flatMap() will do both.
+console.log(flatMappedArray);
+
+// ============== JOIN ==============
+
+console.log("============== JOIN ==============");
+// .join() - joins the elements of an array into a string
+
+let joinedArray = cityArray.join(", ");
+console.log(joinedArray);
+
+// ============== SLICE ==============
+
+console.log("============== SLICE ==============");
+// .slice() - returns a copy of a portion of an array into a NEW array.
+
+let slicedArray = cityArray.slice(2, 3);
+console.log(slicedArray);
+
+// ============== SPLICE ==============
+
+console.log("============== SPLICE ==============");
+// .splice() - removes or replaces existing elements and/or adds new elements in place. Splice changes the ORIGINAL array.
+
+let splicedArray = cityArray.splice(1, 2, "Prilep");
+console.log(splicedArray);
+console.log(cityArray);
+
+// ============== REVERSE ==============
+
+console.log("============== REVERSE ==============");
+// .reverse() - reverses the elements of an array. Reverse changes the ORIGINAL array.
+
+let reversedArray = cityArray.reverse();
+let reversedArray2 = [...cityArray].reverse(); // This will create a copy of the array and reverse it.
+console.log(reversedArray);
+console.log(reversedArray2);
+console.log(cityArray);
+
