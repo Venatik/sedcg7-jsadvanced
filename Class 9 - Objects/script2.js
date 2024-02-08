69f04665553fd3e5fd4070185e483d6e9cda587c
Venatik <stefan.trajkovski008@gmail.com>
2024-02-08 20:31:45 +0100

    new file:   Class 11 - Object Inheritance/index.html 	new file:   Class 11 - Object Inheritance/script.js 	modified:   Class 9 - Objects/script2.js 	modified:   Class 11 - Object Inheritance/index.html 	modified:   Class 11 - Object Inheritance/script.js 	modified:   Class 9 - Objects/index.html

--- Class 9 - Objects/script2.js
+++ Class 9 - Objects/script2.js
@@ -45,3 +45,118 @@ console.log(hobby1, hobby2);
 const [hobby01, , hobby02] = hobbies;
 console.log(hobby01, hobby02);
 
+// ============ Object Methods ============
+
+console.log("============ Object Methods ============");
+
+const dog = {
+    isHappy: true,
+    bark: function () {
+        console.log("Woof! Woof!");
+    }
+}
+
+// ============ Object.create() ============
+console.log("============ Object.create() ============");
+
+// Object create is a method that accepts an object as a parameter and will return a new object with all the inherited entities (properties and methods) of the object that was passed as a parameter
+
+const barnie = Object.create(dog); // Barnie will have everything that dog has
+barnie.name = "Barnie";
+barnie.breed = "Dzukelce";
+barnie.age = 2;
+console.log("========= barnie with create", barnie);
+
+barnie.bark();
+
+// ============ Object.assign() ============
+console.log("============ Object.assign() ============");
+
+// Object assign is a method that can merge two objects into one. It accepts two objects as parameters and will return a new object with all the properties and methods of both objects
+
+const addressInfo = {
+    city: "Skopje",
+    street: "Partizanska",
+    number: 15
+}
+
+const barnieChip = Object.assign(barnie, addressInfo);
+console.log("========= barnie with assign", barnieChip);
+
+const secondChip = {
+    city: "Ohrid",
+    street: "Ohridska",
+    number: 22
+}
+
+const barnieChipUpdate = Object.assign(barnieChip, secondChip);
+console.log("========= barnieChipUpdate", barnieChipUpdate);
+
+// ============ Object.keys() ============
+console.log("============ Object.keys() ============");
+
+// Object keys is a method that accepts an object as a parameter and will return an array of all the keys of the object (key of key-value pair)
+
+const barnieKeys = Object.keys(barnieChipUpdate);
+console.log("========= barnie keys", barnieKeys);
+
+// ============ Object.values() ============
+console.log("============ Object.values() ============");
+
+// Object values returns all values from the object as an array (value of key-value pair)
+
+const barnieValues = Object.values(barnie);
+console.log("========= barnie values", barnieValues);
+
+// ============ Object.entries() ============
+console.log("============ Object.entries() ============");
+
+// Object entries returns all key-value pairs from the object as an array of arrays
+
+const barnieEntries = Object.entries(barnie);
+console.log("========= barnie entries", barnieEntries);
+
+// ============ Object.freeze() ============
+console.log("============ Object.freeze() ============");
+
+// Object freeze is a method that accepts an object as a parameter and will return the same object but with all its properties and methods locked (read-only)
+
+Object.freeze(barnie);
+barnie.name = "Rex";
+barnie.friend = "Sharko";
+console.log("========= frozen barnie", barnie);
+
+
+// ============ Object.seal() ============
+console.log("============ Object.seal() ============");
+
+// Object seal is a method that accepts an object as a parameter and will return the same object but with all its properties locked (read-only). We can change exising properties but we can't add new ones.
+
+const myPuppy = {};
+myPuppy.name = "Johnny";
+console.log(myPuppy);
+Object.seal(myPuppy);
+myPuppy.name = "Rex";
+myPuppy.age = 2;
+console.log("========= sealed puppy", myPuppy);
+
+// ============ Dynamically adding properties ============
+console.log("============ Dynamically adding properties ============");
+
+const newDog = {};
+const nameProperty = "dogName";
+
+newDog[nameProperty] = "Random Dog Name";
+console.log(newDog);
+
+// ============ Spread Operator ============
+console.log("============ Spread Operator ============");
+
+const numbers = [1, 2, 3];
+
+function sum(a, b, c) {
+    return a + b + c;
+}
+
+console.log(sum(...numbers)); // instead of using numbers[0], numbers[1], numbers[2]
+
